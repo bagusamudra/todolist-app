ed73d4a839bd962891234632597065023ce47772
bagusamudra <samudrabagus9@gmail.com>
2023-08-02 09:52:56 +0700

    Add TodolistAppV2

--- src/main/java/test/view/TodoListViewTest.java
+++ src/main/java/test/view/TodoListViewTest.java
@@ -9,7 +9,7 @@ import view.TodoListView;
 public class TodoListViewTest {
 
     public static void main(String[] args) {
-        testShowTodoList();
+        testRemoveTodoList();
     }
 
     public static void testShowTodoList() {
@@ -23,6 +23,36 @@ public class TodoListViewTest {
 
         todoListView.showTodoList();
 
+    }
+
+    public static void testAddTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListView.addTodoList();
+
+        todoListService.showTodoList();
+
+        todoListView.addTodoList();
+
+        todoListService.showTodoList();
+    }
+
+    public static void testRemoveTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListService.addTodoList("Belajar Java Dasar");
+        todoListService.addTodoList("Belajar Java OOP");
+        todoListService.addTodoList("Belajar Java Standard Classes");
+
+        todoListService.showTodoList();
+
+        todoListView.removeTodoList();
+
+        todoListService.showTodoList();
     }
 
 }
--- src/main/java/util/InputUtil.java
+++ src/main/java/util/InputUtil.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InputUtil {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String input(String info) {
         System.out.print(info + " : ");
